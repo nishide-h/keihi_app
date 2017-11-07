ddec72e6dc5283a20394399357941a050df9519d
nishide <nishide.h@gmail.com>
2017-11-07 19:30:53 +0900

    add "CSV出力ボタン"

--- generate_csv.js
+++ generate_csv.js
@@ -1,3 +1,23 @@
 (function() {
   "use strict";
+  
+  kintone.events.on("app.record.index.show", function(event) {
+    if (event.viewId !== 5299939) { // 出力用一覧（当月&出力済除外）ビュー
+      return;
+    }
+
+    if (document.getElementById('btn_csv_output') !== null) {
+      return;
+    }
+
+    var btnCSVOutput = document.createElement('button');
+    btnCSVOutput.id = 'btn_csv_output';
+    btnCSVOutput.innerHTML = 'CSV出力';
+
+    btnCSVOutput.onclick = function() {
+      alert('CSVを出力します');
+    };
+
+    kintone.app.getHeaderMenuSpaceElement().appendChild(btnCSVOutput);
+  });
 })();
